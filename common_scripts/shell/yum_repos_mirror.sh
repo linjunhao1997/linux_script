a20dd2741f865f4b6105406d65ea402a30ee07bd
linjunhao1997 <852857605@qq.com>
2021-05-23 13:38:15 +0800

    add

---
+++ common_scripts/shell/yum_repos_mirror.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+yum -y install wget
+
+mv -f /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
+
+wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+
+yum clean all
+yum makecache
+
+
+rpm -e epel-release
+
+yum -yinstall epel-release
+
+wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
